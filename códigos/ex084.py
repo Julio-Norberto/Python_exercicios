bae5635873b7c3d6dc08c4a2f1178b10e2b1b6f2
Júlio César <juliocsscl@gmail.com>
2021-12-17 18:37:20 -0300

    conteúdo atualizado

--- códigos/ex084.py
+++ códigos/ex084.py
@@ -5,19 +5,22 @@ dados = list()
 pessoas = list()
 pesadas = list()
 leves = list()
-for c in range(0, 6):
+while True:
     nome = str(input('Informe o seu nome: '))
     peso = float(input('Informe seu peso: '))
     tot_pessoas += 1
-    if peso > maior:
+    if peso >= maior:
         maior = peso
         pesadas.append(nome)
-    else:
+    elif peso <= menor:
         leves.append(nome)
     dados.append(nome)
     dados.append(peso)
     pessoas.append(dados[:])
     dados.clear()
+    resp = str(input('Quer continuar? (S / N)'))
+    if resp in 'Nn':
+        break
 print(f'O total de pessoas cadastradas foi de: {tot_pessoas}')
 print(f'As pessoas mais leves são: {leves}')
 print(f'As pessoas mais pesadas são: {pesadas}')
