a0c662a08cfe55c3ad8db7f977f3568d5c92ed4e
Júlio César <juliocsscl@gmail.com>
2021-12-20 18:32:29 -0300

    conteúdo atualizado

--- códigos/ex085.py
+++ códigos/ex085.py
@@ -1,7 +1,7 @@
 valores = list()
 pares = list()
 impares = list()
-for c in range(0, 6):
+for c in range(0, 7):
     v = int(input('Informe um valor: '))
     if v % 2 == 0:
         pares.append(v)
@@ -9,4 +9,7 @@ for c in range(0, 6):
         impares.append(v)
 valores.append(pares[:])
 valores.append(impares[:])
-print(valores)
+valores[0].sort()
+valores[1].sort()
+print(f'Os valores pares são: {valores[0]}')
+print(f'Os valores impares são: {valores[1]}')
