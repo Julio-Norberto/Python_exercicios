1f9b568907feba0d1b9004a13a2ede17d381acbb
Júlio César <juliocsscl@gmail.com>
2021-12-22 13:19:22 -0300

    conteúdo atualizado

---
+++ ex086.py
@@ -0,0 +1,11 @@
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input('Informe um número para preencher a matriz: '))
+
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(matriz[l][c], end=' ')
+    print(' ')
+print(matriz[l][c])
